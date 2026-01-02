a0cca94b2e7a5384457bdfa3444da5e9d2977531
EnzBazar <ikhalil7446@gmail.com>
2026-01-02 22:08:00 +0600

    auto deploy test

--- client/src/Components/AdsBanneraslider/index.jsx
+++ client/src/Components/AdsBanneraslider/index.jsx
@@ -25,9 +25,9 @@ const AdsBannerSlider = ({ data = [] }) => {
         breakpoints={{
           0: { slidesPerView: 1, spaceBetween: 10 },
           480: { slidesPerView: 2, spaceBetween: 10 },
-          768: { slidesPerView: 3, spaceBetween: 15 },
-          1024: { slidesPerView: 4, spaceBetween: 20 },
-          1280: { slidesPerView: 5, spaceBetween: 20 },
+          768: { slidesPerView: 2, spaceBetween: 15 },
+          1024: { slidesPerView: 3, spaceBetween: 20 },
+          1280: { slidesPerView: 3, spaceBetween: 20 },
         }}
         navigation={context?.windowWidth < 992 ? false : true}
         modules={[FreeMode, Navigation, Pagination, Autoplay]}
--- client/src/Components/AdsBannerasliderV2/index.jsx
+++ client/src/Components/AdsBannerasliderV2/index.jsx
@@ -19,9 +19,9 @@ const AdsBannerSliderV2 = ({ data = [] }) => {
         breakpoints={{
           0: { slidesPerView: 1, spaceBetween: 5 },
           480: { slidesPerView: 1, spaceBetween: 5 },
-          768: { slidesPerView: 3, spaceBetween: 15 },
-          1024: { slidesPerView: 4, spaceBetween: 20 },
-          1280: { slidesPerView: 5, spaceBetween: 20 },
+          768: { slidesPerView: 2, spaceBetween: 15 },
+          1024: { slidesPerView: 3, spaceBetween: 20 },
+          1280: { slidesPerView: 3, spaceBetween: 20 },
         }}
         navigation={context?.windowWidth < 992 ? false : true}
         pagination={{ clickable: true }}
