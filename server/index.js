2c995607da00a81333acd3924b1f1105ad4d9859
EnzBazar <ikhalil7446@gmail.com>
2026-01-06 14:40:43 +0600

    update homepage

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 // ---- CORS ----
 app.use(cors({
-    origin: "https://www.goroabazar.com",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
     credentials: true
