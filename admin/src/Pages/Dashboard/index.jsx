73a3ffbd08ba8a05905ded55b9863b40f3e09fea
EnzBazar <ikhalil7446@gmail.com>
2026-01-07 00:14:38 +0600

    update homepage

--- admin/src/Pages/Dashboard/index.jsx
+++ admin/src/Pages/Dashboard/index.jsx
@@ -805,7 +805,7 @@ const getChartData = async () => {
  <div className='w-full py-2 bg-[#f1faff] px-5 border border-[rgba(0,0,0,0.2)] flex items-center gap-8 mb-5 justify-between rounded-md'>
  <div className='info'>
 <h1 className='text-[30px] font-bold leading-10 mb-3'>
-  স্বাগতম ####<br />
+  স্বাগতম<br />
 <span className='text-blue-500 !ml-auto'> {context?.userData?.name || "User"}</span> 
 </h1>
   <p>Here`s what happning on your store today. See the statistics at once.</p>
