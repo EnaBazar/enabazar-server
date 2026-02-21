9e247f405feaa34d5d6da735de22ca447a7084bd
enabazar-server <ikhalil7446@gmail.com>
2026-02-21 23:43:23 +0600

    test update

--- server/libs/db.js
+++ server/libs/db.js
@@ -2,24 +2,39 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config();
 
-
 if(!process.env.MONGODB_URL){
     throw new Error(
-        "Please provide MONGODB_URI in the .env file"
+        "Please provide MONGODB_URL in the .env file"
     )
-    
 }
 
-const DbCon = async()=>{
-    try{
-        mongoose.connect(process.env.MONGODB_URL)
-        console.log('mongodb is connected')
-    }catch(error){
-        console.log('mongodb conection error',error)
-        process.exit(1)
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            keepAlive: true,
+            serverSelectionTimeoutMS: 5000, // retry for 5 seconds
+        });
+        console.log('MongoDB is connected');
+    } catch (error) {
+        console.error('MongoDB initial connection error:', error);
+        process.exit(1);
     }
-    
-    
-}
+};
 
-export default DbCon;
+// Event listeners
+mongoose.connection.on('connected', () => {
+    console.log('MongoDB connection established');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('MongoDB connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('MongoDB disconnected! Reconnecting...');
+    connectDB(); // Reconnect automatically
+});
+
+export default connectDB;
