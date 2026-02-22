021e49978d40110668fc435359f00223faee0f4f
enabazar-server <ikhalil7446@gmail.com>
2026-02-23 00:33:29 +0600

    test update

--- server/utils/sendSMS.js
+++ server/utils/sendSMS.js
@@ -1,55 +1,16 @@
-import http from "http";
-import querystring from "querystring";
+import axios from "axios";
 
-const sendSMS = (mobile, otp) => {
-  return new Promise((resolve, reject) => {
+const sendSMS = async (mobile, otp) => {
+  try {
+    const response = await axios.get(
+      `http://api.greenweb.com.bd/api.php?token=${process.env.GREENWEB_TOKEN }&to=${mobile}&message=Your OTP is ${otp}`
+    );
 
-    const message = `Your OTP is ${otp}`;
-
-    const postData = querystring.stringify({
-      token: process.env.GREENWEB_TOKEN, // .env থেকে নিবে
-      to: mobile,
-      message: message
-    });
-
-    const options = {
-      hostname: "api.bdbulksms.net",
-      path: "/api.php",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Content-Length": postData.length
-      }
-    };
-
-    const req = http.request(options, (res) => {
-
-      let data = "";
-
-      res.on("data", (chunk) => {
-        data += chunk;
-      });
-
-      res.on("end", () => {
-        console.log("SMS Response:", data);
-
-        // bdbulksms success হলে সাধারণত 200 বা specific text দেয়
-        if (data.includes("SMS SUBMITTED")) {
-          resolve(true);
-        } else {
-          resolve(true); // চাইলে এখানে stricter condition দিতে পারো
-        }
-      });
-    });
-
-    req.on("error", (e) => {
-      console.log("SMS Error:", e.message);
-      resolve(false);
-    });
-
-    req.write(postData);
-    req.end();
-  });
+    return response.data;
+  } catch (error) {
+    console.log("SMS Error:", error.message);
+    return false;
+  }
 };
 
 export default sendSMS;
