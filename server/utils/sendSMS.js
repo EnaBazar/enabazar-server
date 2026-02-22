90665c75d2b913a51d8d6101aea61d7ca0465c4f
enabazar-server <ikhalil7446@gmail.com>
2026-02-22 17:45:35 +0600

    test update

--- server/utils/sendSMS.js
+++ server/utils/sendSMS.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
+
+
+
 const sendSMS = async (mobile, otp) => {
   try {
-    const response = await axios.get(
-      `http://api.bdbulksms.net/api.php?token=1310419322517704711457bef66f552f9ed3565718ed5fbb807b1&to=${mobile}&message=Your OTP is ${otp}`
-    );
+    const token = process.env.GREENWEB_TOKEN;
+    const message = encodeURIComponent(`Your OTP is ${otp}`);
+    const url = `https://api.bdbulksms.net/api.php?token=${token}&to=${mobile}&message=${message}`;
 
+    const response = await axios.get(url);
     return response.data;
   } catch (error) {
     console.log("SMS Error:", error.message);
