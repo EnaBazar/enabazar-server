02b467b38fbc4918f079923f5696053d947ba96d
enabazar-server <ikhalil7446@gmail.com>
2026-02-22 17:06:37 +0600

    test update

--- server/utils/sendSMS.js
+++ server/utils/sendSMS.js
@@ -1,14 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const sendSMS = async (mobile, otp) => {
-  const smsData = new URLSearchParams();
-  smsData.append('token', '1310419322517704711457bef66f552f9ed3565718ed5fbb807b1'); // Replace with your actual token
-  smsData.append('to', mobile);
-  smsData.append('message', `Your OTP is: ${otp}`);
-
+const sendSMS = async (mobile, otp) => {
   try {
-    await axios.post('https://api.bdbulksms.net/api.php', smsData);
+    const response = await axios.get(
+      `https://api.bdbulksms.net/api.php?token=1310419322517704711457bef66f552f9ed3565718ed5fbb807b1&to=${mobile}&message=Your OTP is ${otp}`
+    );
+
+    return response.data;
   } catch (error) {
-    console.error('Error sending OTP:', error);
+    console.log("SMS Error:", error.message);
+    return false;
   }
 };
+
+export default sendSMS;
