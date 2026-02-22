ba93d74cc7716a67c78e91f3bbc7135d11cb5c8c
enabazar-server <ikhalil7446@gmail.com>
2026-02-22 17:10:03 +0600

    test update

--- server/utils/sendSMS.js
+++ server/utils/sendSMS.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const sendSMS = async (mobile, otp) => {
   try {
     const response = await axios.get(
-      `https://api.bdbulksms.net/api.php?token=1310419322517704711457bef66f552f9ed3565718ed5fbb807b1&to=${mobile}&message=Your OTP is ${otp}`
+      `http://api.bdbulksms.net/api.php?token=1310419322517704711457bef66f552f9ed3565718ed5fbb807b1&to=${mobile}&message=Your OTP is ${otp}`
     );
 
     return response.data;
