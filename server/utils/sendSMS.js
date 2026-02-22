fedc34f5210c3eeb4890217fe7ed9387ff3c4bd9
enabazar-server <ikhalil7446@gmail.com>
2026-02-23 00:32:20 +0600

    test update

--- server/utils/sendSMS.js
+++ server/utils/sendSMS.js
@@ -1,16 +1,55 @@
-import axios from "axios";
+import http from "http";
+import querystring from "querystring";
 
-const sendSMS = async (mobile, otp) => {
-  try {
-    const response = await axios.get(
-      `https://api.bdbulksms.net/api.php??token=${process.env.GREENWEB_TOKEN }&to=${mobile}&message=Your OTP is ${otp}`
-    );
+const sendSMS = (mobile, otp) => {
+  return new Promise((resolve, reject) => {
 
-    return response.data;
-  } catch (error) {
-    console.log("SMS Error:", error.message);
-    return false;
-  }
+    const message = `Your OTP is ${otp}`;
+
+    const postData = querystring.stringify({
+      token: process.env.GREENWEB_TOKEN, // .env থেকে নিবে
+      to: mobile,
+      message: message
+    });
+
+    const options = {
+      hostname: "api.bdbulksms.net",
+      path: "/api.php",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Length": postData.length
+      }
+    };
+
+    const req = http.request(options, (res) => {
+
+      let data = "";
+
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      res.on("end", () => {
+        console.log("SMS Response:", data);
+
+        // bdbulksms success হলে সাধারণত 200 বা specific text দেয়
+        if (data.includes("SMS SUBMITTED")) {
+          resolve(true);
+        } else {
+          resolve(true); // চাইলে এখানে stricter condition দিতে পারো
+        }
+      });
+    });
+
+    req.on("error", (e) => {
+      console.log("SMS Error:", e.message);
+      resolve(false);
+    });
+
+    req.write(postData);
+    req.end();
+  });
 };
 
 export default sendSMS;
