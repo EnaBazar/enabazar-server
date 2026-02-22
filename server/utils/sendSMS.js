0cfcc2b0b8ac2b2015648e2db8fcbb269957dabb
enabazar-server <ikhalil7446@gmail.com>
2026-02-22 19:13:28 +0600

    test update

--- server/utils/sendSMS.js
+++ server/utils/sendSMS.js
@@ -1,28 +1,21 @@
 import axios from "axios";
 
-const sendSMS = async (mobile, otp) => {
+export const sendSMS = async (mobile, otp) => {
   try {
-    if (!process.env.SMS_API_KEY) {
-      throw new Error("SMS_API_KEY is not defined in environment variables");
-    }
+    const params = new URLSearchParams();
+    params.append("token", process.env.GREENWEB_TOKEN);
+    params.append("to", mobile);
+    params.append("message", `Your OTP is ${otp}`);
 
-    const message = encodeURIComponent(`Your OTP is ${otp}`);
+    const response = await axios.post(
+      "https://api.bdbulksms.net/api.php",
+      params
+    );
 
-    const url = `https://api.bdbulksms.net/api.php?token=1310419322517704711457bef66f552f9ed3565718ed5fbb807b1&to=${mobile}&message=test%20massage`;
-
-    const response = await axios.get(url);
-
-    if (response.data) {
-      console.log(`OTP sent to ${mobile}: ${otp}`);
-      return response.data;
-    } else {
-      console.log(`Failed to send OTP to ${mobile}`);
-      return false;
-    }
+    console.log("SMS Response:", response.data);
+    return response.data;
   } catch (error) {
-    console.error("SMS Error:", error.message);
+    console.error("SMS Error:", error.response?.data || error.message);
     return false;
   }
 };
-
-export { sendSMS };
