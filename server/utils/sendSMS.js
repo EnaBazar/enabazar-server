fb5eaa58c8baf5446345e38da37245f331b134f0
enabazar-server <ikhalil7446@gmail.com>
2026-02-22 19:02:20 +0600

    test update

--- server/utils/sendSMS.js
+++ server/utils/sendSMS.js
@@ -8,7 +8,7 @@ const sendSMS = async (mobile, otp) => {
 
     const message = encodeURIComponent(`Your OTP is ${otp}`);
 
-    const url = `https://api.bdbulksms.net/api.php?token=1310419322517704711457bef66f552f9ed3565718ed5fbb807b1&to=01674847446&message=test%20massage`;
+    const url = `https://api.bdbulksms.net/api.php?token=1310419322517704711457bef66f552f9ed3565718ed5fbb807b1&to=${mobile}&message=test%20massage`;
 
     const response = await axios.get(url);
 
