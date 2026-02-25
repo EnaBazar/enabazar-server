3b9b71a35193d9336f56043be7c29647edf49ef3
enabazar-server <ikhalil7446@gmail.com>
2026-02-26 00:55:11 +0600

    test update

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -126,13 +126,13 @@ export async function verifyMobileOtp(req, res) {
     // 🔥 Generate Tokens
     const accesstoken = jwt.sign(
       { id: user._id, role: user.role },
-      process.env.ACCESS_TOKEN_SECRET,
+      process.env.SECRET_KEY_ACCESS_TOKEN,
       { expiresIn: "1d" }
     );
 
     const refreshtoken = jwt.sign(
       { id: user._id },
-      process.env.REFRESH_TOKEN_SECRET,
+      process.env.SECRET_KEY_REFRESH_TOKEN,
       { expiresIn: "7d" }
     );
 
