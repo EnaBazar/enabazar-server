536ad20d3a0338dba5d32b7b29bccef20ec3ed0e
enabazar-server <ikhalil7446@gmail.com>
2026-03-13 06:57:02 +0600

    test update

--- server/route/reportRoute.js
+++ server/route/reportRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { getReport } from "../controllers/reportController";
+import { getReport } from "../controllers/reportController.js";
+;
 
 
 const reportrouter = express.Router();
